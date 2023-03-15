56d0b21d639d255e2ba18f16b4ee804ab43f758c
Devin Benninghoven <devinbenninghoven@gmail.com>
2023-03-15 10:46:17 -0700

    changed path

--- automation/tests/main.py
+++ automation/tests/main.py
@@ -2,35 +2,39 @@ import subprocess
 import os
 import sys
 
-cwd = os.getcwd()
-parentPath = os.path.dirname(cwd)
-print(parentPath)
-sys.path.insert(0, parentPath)
+#cmd += f"sendtext.applescript {phoneNumber} {a.name}"
+
+projpath = "/Users/devin/Sandbox/dsl/automation"
+sys.path.append(projpath)
 
 from src.activitycontainer import ActivityContainer
 from src.testinternetconnection import TestInternetConnection
 from src.currenttime import CurrentTime
 
-urlFilePath = "../data/SecretCalendarLinks.txt"
-phoneNumberPath = "../data/phonenumber.txt"
+urlFilePath = projpath + "/data/SecretCalendarLinks.txt"
+phoneNumberPath = projpath + "/data/phonenumber.txt"
 with open(phoneNumberPath, 'r') as file:
     phoneNumber = file.read()
 
 def NameToCommand(name: str) -> str:
     cmd = "osascript "
-    cmd += parentPath
+    cmd += projpath
     cmd += "/src/scripts/"
     if name == "Leet Code":
         cmd += "launchgooglewebsite.applescript"
         # arg here
     elif name == "":
+        cmd = ""
         pass
     else:
-        cmd += f"sendtext.applescript {phoneNumber} {a.name}"
+        cmd = ""
+        pass
     return cmd
 
 # Execute the command
 def ExecuteCmd(command):
+    if not command:
+        return
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 
 
@@ -41,6 +45,7 @@ if __name__ == '__main__':
         todaysActivities = ac.TodaysActivities()
         if len(todaysActivities):
             for a in todaysActivities:
+                print(a)
                 cmd = NameToCommand(a.calName)
                 ExecuteCmd(cmd)
     else:
